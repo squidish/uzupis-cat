1f5d97576bd61531acbb9275ff8236775330b217
squidish <christopherfwilliams@yahoo.co.uk>
2025-09-09 18:21:21 +0100

    tweeked tests for npm

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     baseURL: 'http://localhost:5173',
   },
   webServer: {
-    command: 'pnpm -w dev',
+    command: 'npm -w dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
